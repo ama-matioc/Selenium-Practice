c015531744d0b651f3dac4948b3ec83f44df593c
ama-matioc <matiocamalia@gmail.com>
2025-09-17 20:15:57 +0300

    work with tables

---
+++ src/main/java/com/demoqa/pages/elements/ElementsPage.java
@@ -0,0 +1,15 @@
+package com.demoqa.pages.elements;
+
+import com.demoqa.pages.HomePage;
+import com.demoqa.pages.elements.WebTablesPage;
+import org.openqa.selenium.By;
+
+public class ElementsPage extends HomePage {
+    private By webTablesMenuItem = By.xpath("//li[@id='item-3']/span[text()='Web Tables']");
+
+    public WebTablesPage clickWebTables(){
+        click(webTablesMenuItem);
+        return new WebTablesPage();
+    }
+
+}
