6cea66304b392cd9bc712a2a3f193e16c373e719
ama-matioc <matiocamalia@gmail.com>
2025-09-23 19:37:54 +0300

    confirmation alert

--- src/main/java/com/demoqa/pages/alerts_frames_windows/AlertsPage.java
+++ src/main/java/com/demoqa/pages/alerts_frames_windows/AlertsPage.java
@@ -4,7 +4,15 @@ import org.openqa.selenium.By;
 
 public class AlertsPage extends AlertsFramesWindowsPage{
     private By informationAlertButton= By.id("alertButton");
+    private By confirmAlertButton= By.id("confirmButton");
+    private By confirmationResult= By.id("confirmResult");
     public void clickInformationAlertButton(){
         click(informationAlertButton);
+    }
+    public void clickConfirmAlertButton(){
+        click(confirmAlertButton);
+    }
+    public String getConfirmationResult(){
+        return find(confirmationResult).getText();
     }
 }
--- src/test/java/part3_4/com/demoqa/tests/part4/alerts/AlertsTest.java
+++ src/test/java/part3_4/com/demoqa/tests/part4/alerts/AlertsTest.java
@@ -5,15 +5,24 @@ import org.testng.annotations.Test;
 import part3_4.com.demoqa.base.BaseTest;
 
 import static utilities.SwitchToUtility.*;
-
+@Test
 public class AlertsTest extends BaseTest {
-    @Test
+
     public void testInformationAlert(){
         var alertPage= homePage.goToAlertsFramesWindows().clickAlerts();
         alertPage.clickInformationAlertButton();
         String expectedText="You clicked a button";
         Assert.assertEquals(getAlertText(), expectedText, "Actual and expected message don't match");
         acceptAlert();
+
+    }
+
+    public void testConfirmationAlert(){
+        var alertPage= homePage.goToAlertsFramesWindows().clickAlerts();
+        alertPage.clickConfirmAlertButton();
+        dismissAlert();
+        String expectedResult="You selected Cancel";
+        Assert.assertEquals(alertPage.getConfirmationResult(), expectedResult, "You didn't select cancel ");
 
     }
 }
